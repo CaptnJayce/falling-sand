1f3888e484e948f201054a6d14ce2a4d65b702ef
captnjayce <captn.dev@protonmail.com>
2025-06-30 21:04:30 +0100

    added wood type and colour variation

--- main.cpp
+++ main.cpp
@@ -1,60 +1,93 @@
+#include <algorithm>
 #include <raylib.h>
 
 enum CellType {
     Empty,
     Sand,
+    Wood,
 };
 
 struct Cell {
     int cell_w = 4;
     int cell_h = 4;
 
+    static Color GenerateColour(Color c) {
+        int variation = GetRandomValue(-15, 15);
+
+        c.r = std::clamp(c.r + variation, c.r - 15, c.r + 15);
+        c.g = std::clamp(c.g + variation, c.g - 15, c.g + 15);
+        c.b = std::clamp(c.b + variation / 2, c.b - 10, c.b + 10);
+
+        return c;
+    }
+
     static Color GetColour(CellType type) {
         switch (type) {
         case Empty:
             return BLACK;
         case Sand:
-            return YELLOW;
+            return GenerateColour({194, 178, 128, 255});
+        case Wood:
+            return GenerateColour({63, 29, 11, 255});
         default:
-            return RED; // error
+            return RED;
         }
     }
 };
 
 class Grid {
   public:
-    int rows = 200;
+    int row = 200;
     int col = 400;
     CellType *cells;
+    Color *cell_colours;
 
-    Grid() { cells = new CellType[rows * col]; }
+    Grid() {
+        cells = new CellType[row * col];
+        cell_colours = new Color[row * col];
+        for (int i = 0; i < row * col; i++) {
+            cells[i] = Empty;
+            cell_colours[i] = BLACK;
+        }
+    }
 
-    Color GetCellColour(int x, int y) { return Cell::GetColour(cells[y * col + x]); }
+    ~Grid() {
+        delete[] cells;
+        delete[] cell_colours;
+    }
+
+    Color GetCellColour(int x, int y) {
+        if (cells[y * col + x] == Empty) {
+            return BLACK;
+        }
+        return cell_colours[y * col + x];
+    }
 
     void Update() {
-        for (int y = Grid::rows - 2; y >= 0; y--) {
-            for (int x = 0; x < Grid::col; x++) {
+        for (int y = row - 2; y >= 0; y--) {
+            for (int x = 0; x < col; x++) {
                 if (cells[y * col + x] == Sand) {
                     if (cells[(y + 1) * col + x] == Empty) {
                         cells[(y + 1) * col + x] = Sand;
+                        cell_colours[(y + 1) * col + x] = cell_colours[y * col + x];
                         cells[y * col + x] = Empty;
                     } else {
-                        bool moved = false;
-
-                        // not necessary to randomise direction but sand piles won't look "natural" otherwise
                         int d = GetRandomValue(0, 1) ? 1 : -1;
 
-                        // first, if not second
                         if (x + d >= 0 && x + d < col && cells[(y + 1) * col + (x + d)] == Empty) {
                             cells[(y + 1) * col + (x + d)] = Sand;
+                            cell_colours[(y + 1) * col + (x + d)] = cell_colours[y * col + x];
                             cells[y * col + x] = Empty;
-                            moved = true;
                         } else if (x - d >= 0 && x - d < col && cells[(y + 1) * col + (x - d)] == Empty) {
                             cells[(y + 1) * col + (x - d)] = Sand;
+                            cell_colours[(y + 1) * col + (x - d)] = cell_colours[y * col + x];
                             cells[y * col + x] = Empty;
-                            moved = true;
                         }
                     }
+                }
+
+                if (cells[y * col + x] == Wood) {
+                    // wood behaviour
                 }
             }
         }
@@ -68,14 +101,20 @@ int main() {
     SetTargetFPS(60);
     InitWindow(screen_width, screen_height, "Falling Sand");
 
-    // objects
     Grid g;
     Cell c;
-
-    int cursor_size = 10; // TODO: allow user to change via GUI
+    int cursor_size = 4;
+    int current_type = 0;
 
     while (!WindowShouldClose()) {
         Vector2 mouse_pos = GetMousePosition();
+
+        if (IsKeyPressed(KEY_ONE)) {
+            current_type = 0;
+        }
+        if (IsKeyPressed(KEY_TWO)) {
+            current_type = 1;
+        }
 
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
             int grid_x = mouse_pos.x / c.cell_h;
@@ -83,35 +122,45 @@ int main() {
 
             for (int y = grid_y - cursor_size / 2; y <= grid_y + cursor_size / 2; y++) {
                 for (int x = grid_x - cursor_size / 2; x <= grid_x + cursor_size / 2; x++) {
-                    if (x >= 0 && x < g.col && y >= 0 && y < g.rows) {
-                        g.cells[y * g.col + x] = Sand;
+                    if (current_type == 0) {
+                        if (x >= 0 && x < g.col && y >= 0 && y < g.row && g.cells[y * g.col + x] != Wood) {
+                            int d = GetRandomValue(0, 10);
+                            if (d == 0) {
+                                g.cells[y * g.col + x] = Sand;
+                                g.cell_colours[y * g.col + x] = Cell::GetColour(Sand);
+                            }
+                        }
+                    }
+
+                    if (current_type == 1) {
+                        if (x >= 0 && x < g.col && y >= 0 && y < g.row) {
+                            g.cells[y * g.col + x] = Wood;
+                            g.cell_colours[y * g.col + x] = Cell::GetColour(Wood);
+                        }
                     }
                 }
             }
         }
 
         if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-            Vector2 mouse_pos = GetMousePosition();
             int grid_x = mouse_pos.x / c.cell_h;
             int grid_y = mouse_pos.y / c.cell_w;
 
             for (int y = grid_y - cursor_size / 2; y <= grid_y + cursor_size / 2; y++) {
                 for (int x = grid_x - cursor_size / 2; x <= grid_x + cursor_size / 2; x++) {
-                    if (x >= 0 && x < g.col && y >= 0 && y < g.rows) {
+                    if (x >= 0 && x < g.col && y >= 0 && y < g.row) {
                         g.cells[y * g.col + x] = Empty;
                     }
                 }
             }
         }
 
-        // update
         g.Update();
 
-        // draw
         BeginDrawing();
         ClearBackground(BLACK);
 
-        for (int y = 0; y < g.rows; y++) {
+        for (int y = 0; y < g.row; y++) {
             for (int x = 0; x < g.col; x++) {
                 DrawRectangle(x * c.cell_w, y * c.cell_h, c.cell_w, c.cell_h, g.GetCellColour(x, y));
             }
@@ -120,4 +169,6 @@ int main() {
         DrawFPS(10, 10);
         EndDrawing();
     }
+
+    return 0;
 }
